c965d2ed3115bba208f5d60695197f36454beebe
ahaddad98 <amine.haddadd@gmail.com>
2021-05-14 19:01:29 +0000

    first push

---
+++ day00/sample.class.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sample.class.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 11:35:29 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/05/14 11:48:35 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "sample.class.hpp"
+
+Sample::Sample(void)
+{
+	std::cout << "lazemna l3a9a" << std::endl;
+	return;
+}
+
+Sample::~Sample(void)
+{
+	std::cout << "waaaaaaa waaaa waaa l3aa9aa" << std::endl;
+	return;
+}
