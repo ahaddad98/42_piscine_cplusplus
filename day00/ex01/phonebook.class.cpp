54bb6b5e62051a704b8aad9b8b34b5ec1d126240
ahaddad98 <amine.haddadd@gmail.com>
2021-05-17 17:03:35 +0100

    hhhh nice front

--- day00/ex01/phonebook.class.cpp
+++ day00/ex01/phonebook.class.cpp
@@ -16,27 +16,27 @@
 void Phonebook::init_args(Phonebook *ph_book)
 {
     std::cin.ignore();
-    std::cout << "donnez first name " << std::endl;
+    std::cout << "First name " << std::endl;
     getline(std::cin, ph_book->first_name);
-    std::cout << "donnez last name \n";
+    std::cout << "Last name \n";
     getline(std::cin, ph_book->last_name);
-    std::cout << "donnez nickname \n";
+    std::cout << "Nickname \n";
     getline(std::cin, ph_book->nickname);
-    std::cout << "donnez login \n";
+    std::cout << "Login \n";
     getline(std::cin, ph_book->login);
-    std::cout << "donnez email adress \n";
+    std::cout << "Email adress \n";
     getline(std::cin, ph_book->email_adress);
-    std::cout << "donnez favorite meal \n";
+    std::cout << "Favorite meal \n";
     getline(std::cin, ph_book->favorite_meal);
-    std::cout << "donnez underwear color\n";
+    std::cout << "Underwear color\n";
     getline(std::cin, ph_book->underwear_color);
-    std::cout << "donnez darkest scret\n";
+    std::cout << "Darkest scret\n";
     getline(std::cin, ph_book->darkest_secret);
-    std::cout << "donnez postal adress\n";
+    std::cout << "Postal adress\n";
     getline(std::cin, ph_book->postal_adress);
-    std::cout << "donnez phone number\n";
+    std::cout << "Phone number\n";
     getline(std::cin, ph_book->phone_number);
-    std::cout << "donnez birthday date\n";
+    std::cout << "Birthday date\n";
     getline(std::cin, ph_book->birthday_date);
 }
 
@@ -103,16 +103,16 @@ void Phonebook::search(Phonebook *ph_book, int i)
             std::cout << k << "\n";
             if (k > 0 && k < (i+1))
             {
-                std::cout << ph_book[k - 1].last_name << "\n";
-                std::cout << ph_book[k - 1].nickname << "\n";
-                std::cout << ph_book[k - 1].login << "\n";
-                std::cout << ph_book[k - 1].email_adress << "\n";
-                std::cout << ph_book[k - 1].favorite_meal << "\n";
-                std::cout << ph_book[k - 1].underwear_color << "\n";
-                std::cout << ph_book[k - 1].darkest_secret << "\n";
-                std::cout << ph_book[k - 1].postal_adress << "\n";
-                std::cout << ph_book[k - 1].phone_number << "\n";
-                std::cout << ph_book[k - 1].birthday_date << "\n";
+                std::cout <<"First name       : " <<ph_book[k - 1].last_name << "\n";
+                std::cout <<"Last name        : " <<ph_book[k - 1].nickname << "\n";
+                std::cout <<"Nickname         : " <<ph_book[k - 1].login << "\n";
+                std::cout <<"Email adress     : " <<ph_book[k - 1].email_adress << "\n";
+                std::cout <<"Favorite_meal    : " <<ph_book[k - 1].favorite_meal << "\n";
+                std::cout <<"Underwear_color  : " <<ph_book[k - 1].underwear_color << "\n";
+                std::cout <<"Darkest_secret   : " <<ph_book[k - 1].darkest_secret << "\n";
+                std::cout <<"Postal_adress    : " <<ph_book[k - 1].postal_adress << "\n";
+                std::cout <<"Phone_number     : " <<ph_book[k - 1].phone_number << "\n";
+                std::cout <<"Birthday_date    : " <<ph_book[k - 1].birthday_date << "\n";
             }
         }
         else if (k <= 0 || k > (i + 1))
