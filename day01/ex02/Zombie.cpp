53d25ffdce4300544b06d0361fe7dd73025a8fd5
ahaddad98 <amine.haddadd@gmail.com>
2021-05-22 18:40:30 +0100

    ex02 ...

--- day01/ex02/Zombie.cpp
+++ day01/ex02/Zombie.cpp
@@ -6,7 +6,29 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 20:21:53 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/05/21 20:21:54 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/05/22 18:34:00 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+class Zombie
+{
+private:
+    /* data */
+public:
+    Zombie(/* args */);
+    ~Zombie();
+};
+
+Zombie::Zombie(/* args */)
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+
+#endif
--- day01/ex02/ZombieEvent.hpp
+++ day01/ex02/ZombieEvent.hpp
@@ -6,7 +6,29 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 20:22:01 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/05/21 20:22:02 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/05/22 18:37:14 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIEEVENT_H
+# define ZOMBIEEVENT_H
+
+class ZombieEvent
+{
+private:
+    /* data */
+public:
+    ZombieEvent(/* args */);
+    ~ZombieEvent();
+};
+
+ZombieEvent::ZombieEvent(/* args */)
+{
+}
+
+ZombieEvent::~ZombieEvent()
+{
+}
+
+
+#endif
