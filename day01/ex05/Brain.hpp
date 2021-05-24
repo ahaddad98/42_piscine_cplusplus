5be7b582eff751895b41a3c96d8a959dc53da749
ahaddad98 <amine.haddadd@gmail.com>
2021-05-24 19:34:55 +0100

    ex05 ...

---
+++ day01/ex05/Brain.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 19:31:54 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/05/24 19:34:11 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_H
+# define BRAIN_H
+# include "iostream"
+
+class Brain
+{
+private:
+    /* data */
+public:
+    Brain(/* args */);
+    ~Brain();
+};
+
+Brain::Brain(/* args */)
+{
+}
+
+Brain::~Brain()
+{
+}
+
+
+#endif
---
+++ day01/ex05/Human.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Human.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 19:31:59 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/05/24 19:34:04 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef HUMAN_H
+# define HUMAN_H
+# include "iostream"
+class Human
+{
+private:
+    /* data */
+public:
+    Human(/* args */);
+    ~Human();
+};
+
+Human::Human(/* args */)
+{
+}
+
+Human::~Human()
+{
+}
+
+
+#endif
