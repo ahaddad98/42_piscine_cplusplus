fde35eb73328d06f21c4db15f93ec36260a9873d
ahaddad98 <amine.haddadd@gmail.com>
2021-06-04 19:02:17 +0100

    encore ex03 .

---
+++ day03/ex03/NinjaTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   NinjaTrap.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/04 19:00:21 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/06/04 19:01:40 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NINJATRAP_H
+# define NINJATRAP_H
+class NinjaTrap
+{
+private:
+    /* data */
+public:
+    NinjaTrap(/* args */);
+    ~NinjaTrap();
+};
+
+
+
+#endif
