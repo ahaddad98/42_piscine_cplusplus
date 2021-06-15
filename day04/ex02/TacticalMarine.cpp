3fdf2ebe781b6c7704e5187984c291531767ea41
ahaddad98 <amine.haddadd@gmail.com>
2021-06-15 21:53:27 +0100

    day04 ex02 encore.

--- day04/ex02/TacticalMarine.cpp
+++ day04/ex02/TacticalMarine.cpp
@@ -6,7 +6,7 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 00:47:09 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/06/15 00:47:28 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/06/15 21:50:30 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,20 @@ TacticalMarine::TacticalMarine(/* args */)
 
 TacticalMarine::~TacticalMarine()
 {
+}
+
+ISpaceMarine *TacticalMarine::clone() const
+{
+}
+void TacticalMarine::battleCry() const
+{
+    std::cout << "For the holy PLOT!" << std::endl;
+}
+void TacticalMarine::rangedAttack() const
+{
+    std::cout <<  "* attacks with a bolter *" << std::endl;
+}
+void TacticalMarine::meleeAttack() const
+{
+    std::cout << "* attacks with chainfists *" << std::endl;
 }
--- day04/ex02/TacticalMarine.hpp
+++ day04/ex02/TacticalMarine.hpp
@@ -6,20 +6,25 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 00:46:02 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/06/15 00:47:26 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/06/15 21:49:01 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TACTCALMARINE_H
-# define TACTCALMARINE_H
-
-class TacticalMarine
+#define TACTCALMARINE_H
+#include "ISpaceMarine.hpp"
+#include <iostream>
+class TacticalMarine : public ISpaceMarine
 {
 private:
     /* data */
 public:
     TacticalMarine(/* args */);
     ~TacticalMarine();
+    ISpaceMarine *clone() const;
+    void battleCry() const;
+    void rangedAttack() const;
+    void meleeAttack() const;
 };
 
 #endif
