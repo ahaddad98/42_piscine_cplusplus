73cdd0ce2b7469723c4151fed31816abd4400e5a
Amine Haddad <ahaddad@Amines-MacBook-Air.local>
2021-06-17 00:13:32 +0100

    day04 ex03 first Abstruct class

--- day04/ex03/AMateria.hpp
+++ day04/ex03/AMateria.hpp
@@ -6,21 +6,28 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 23:46:02 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/06/16 23:47:13 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/06/17 00:13:02 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AMATERIA_H
 # define AMATERIA_H
 #include <iostream>
-
+#include "ICharacter.hpp"
 class AMateria
 {
 private:
-    /* data */
+    std::string const & type;
+    unsigned int _xp;
+    
 public:
-    AMateria(/* args */);
-    ~AMateria();
+    AMateria();
+    virtual ~AMateria();
+    AMateria(std::string const & type);
+    std::string const & getType() const;
+    unsigned int getXP() const;
+    virtual AMateria* clone() const = 0;
+    virtual void use(ICharacter& target);
 };
 
 #endif
