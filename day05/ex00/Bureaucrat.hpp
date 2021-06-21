41ea75dfa14d7ef388cfb02bfc072dddc65b72b1
Amine Haddad <ahaddad@Amines-MacBook-Air.local>
2021-06-22 00:23:21 +0100

    day05  ex00 ...

---
+++ day05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/22 00:20:09 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/06/22 00:21:30 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT
+# define BUREAUCRAT
+# include <iostream>
+class Bureaucrat
+{
+private:
+    /* data */
+public:
+    Bureaucrat(/* args */);
+    ~Bureaucrat();
+};
+
+#endif
