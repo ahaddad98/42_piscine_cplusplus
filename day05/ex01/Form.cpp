aa45252def78d456141600d166bf0c6047f4829b
ahaddad98 <amine.haddadd@gmail.com>
2021-06-24 21:08:03 +0100

    day05 ex01 add be signed and the other function in Bureaucrate

--- day05/ex01/Form.cpp
+++ day05/ex01/Form.cpp
@@ -6,13 +6,13 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 16:33:56 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/06/24 17:48:08 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/06/24 21:04:56 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form(std::string _name , int _gra_sign, int _gra_exec) : name(_name), grade_signed(_gra_sign), grade_executer(_gra_exec)
+Form::Form(std::string _name, int _gra_sign, int _gra_exec) : name(_name), grade_signed(_gra_sign), grade_executer(_gra_exec)
 {
     signe = false;
 }
@@ -21,19 +21,30 @@ Form::~Form()
 {
 }
 
-int Form::getgrade_signed() const 
+Form::Form(Form const &form)
+{
+    signe = form.signe;
+}
+
+Form &Form::operator=(Form const & form)
+{
+    signe = form.signe;
+    return *this;
+}
+
+int Form::getgrade_signed() const
 {
     return grade_signed;
 }
-int Form::getgrade_executer() const 
+int Form::getgrade_executer() const
 {
     return grade_executer;
 }
-std::string Form::getname() const 
+std::string Form::getname() const
 {
     return name;
 }
-std::ostream & operator<<(std::ostream & os , const Form & form)
+std::ostream &operator<<(std::ostream &os, const Form &form)
 {
     os << "amine " << form.getname() << std::endl;
     return os;
--- day05/ex01/Form.hpp
+++ day05/ex01/Form.hpp
@@ -6,30 +6,40 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 16:32:20 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/06/24 17:48:00 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/06/24 21:07:23 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FORM_H
 # define FORM_H
-#include <iostream>
-//  using namespace std;
+# include <iostream>
+# include "Bureaucrat.hpp"
+# include <stdexcept>
 class Form
 {
 private:
     std::string const name;
-    bool signe ;
-    int  const grade_signed;
-    int  const grade_executer;
+    int   grade_signed;// const
+    int   grade_executer;// const
+    bool signe;
 public:
     Form(/* args */);
     Form(std::string _name , int _gra_sign, int _gra_exec);
-    Form(const Form  & form);
-    Form & operator=(const Form & form);
+    Form(Form const &form);
+    Form & operator=(Form const &form);
     ~Form();
     int getgrade_signed() const ;
     int getgrade_executer() const ;
     std::string getname() const ;
+    void beSigned(Bureaucrat const & bur);
+    class GradeTooHighException :  public std::exception 
+    {
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException :  public std::exception 
+    {
+        virtual const char *what() const throw();
+    };
 };
 
 std::ostream & operator<<(std::ostream os , const Form & form);
