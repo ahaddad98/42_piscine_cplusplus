94e8601f408a2057d100cae531b1d471abc5dc9f
ahaddad98 <amine.haddadd@gmail.com>
2021-06-30 12:56:36 +0100

    day06 ex00 just protect the argument

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -6,12 +6,24 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 11:58:45 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/06/30 11:21:06 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/06/30 12:52:05 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstring>
+#include <stdexcept>
+
+class nan_cast_to_float : public std::exception
+{
+    virtual const char *what() const throw();
+};
+
+const char *nan_cast_to_float::what() const throw()
+{
+    return "nan";
+}
+
 int main(int ac, char **av)
 {
     if (ac != 2)
@@ -24,28 +36,64 @@ int main(int ac, char **av)
     is_point = str.find('.');
     // std::cout << is_point << std::endl;
     int is_decimale = 0;
-    int i  = 0;
+    int i = 0;
     if (is_point != -1)
     {
         while (str[i] && isdigit(str[i]))
         {
-            i++;
             is_decimale++;
+            i++;
         }
     }
+    else
+    {
+        is_decimale = 1;
+    }
     char _char;
-    // int _int;
-    // float _float;
-    // double _double;
+    int _int;
+    float _float;
+    double _double;
+    std::cout.precision(is_decimale);
+    std::cout.setf(std::ios::fixed);
     try
     {
         _char = static_cast<char>(std::stoi(str));
-        std::cout << "cast to char :" << _char << std::endl;
+        if (_char <= 32)
+            std::cout << "char: non displayable" << std::endl;
+        else
+            std::cout << "char :" << _char << std::endl;
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << "char: impossible" << '\n';
     }
-    
+    try
+    {
+        _int = static_cast<int>(std::stoi(str));
+        std::cout << "int :" << _int << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "int: impossible" << '\n';
+    }
+    try
+    {
+        _float = static_cast<float>(std::stof(str));
+        std::cout << "float :" << _float << "f" << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "float: nanf" << '\n';
+    }
+    try
+    {
+        _double = static_cast<double>(std::stod(str));
+        std::cout << "double :" << _double << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "double: nan" << '\n';
+    }
+
     return 0;
 }
