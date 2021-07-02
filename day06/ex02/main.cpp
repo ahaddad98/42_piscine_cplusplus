29199502b22398053d99170ad7741d725a3ac5d2
ahaddad98 <amine.haddadd@gmail.com>
2021-07-02 13:48:47 +0100

    day06 ex02 Done

--- day06/ex02/main.cpp
+++ day06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:41:55 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/02 13:12:24 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/02 13:48:29 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,24 @@ Base *generate(void)
     return NULL;
 }
 
-void identify(Base *p)
+void identify_pointer(Base *p)
 {
-    A *_a = dynamic_cast<A *>(p);
+    A *_a;
+    _a= dynamic_cast<A *>(p);
     if (_a != NULL)
     {
         std::cout << "A" << std::endl;
         return;
     }
-    B *_b = dynamic_cast<B *>(p);
+    B *_b;
+    _b = dynamic_cast<B *>(p);
     if (_b != NULL)
     {
         std::cout << "B" << std::endl;
         return;
     }
-    C *_c = dynamic_cast<C *>(p);
+    C *_c;
+    _c = dynamic_cast<C *>(p);
     if (_c != NULL)
     {
         std::cout << "C" << std::endl;
@@ -49,44 +52,51 @@ void identify(Base *p)
     std::cout << "Not A not B and not C" << std::endl;
 }
 
-void identify(Base &p)
+void identify_refernce(Base &p)
 {
     try
     {
         A &_a = dynamic_cast<A &>(p);
+        (void)_a;
         std::cout << "A" << std::endl;
-        ;
     }
     catch (std::bad_cast &e)
     {
         std::cerr << e.what() << std::endl;
-        ;
     }
     try
     {
         B &_b = dynamic_cast<B &>(p);
+        (void)_b;
         std::cout << "B" << std::endl;
-        ;
     }
     catch (std::bad_cast &e)
     {
         std::cerr << e.what() << std::endl;
-        ;
     }
     try
     {
         C &_c = dynamic_cast<C &>(p);
+        (void)_c;
         std::cout << "C" << std::endl;
-        ;
     }
     catch (std::bad_cast &e)
     {
         std::cerr << e.what() << std::endl;
-        ;
     }
 }
 
 int main()
 {
+    std::cout << "/**************** Random instanciate ************************/" << std::endl;
+    std::cout << generate() << std::endl;
+   std::cout << "/***************** identifypointer **************************/" << std::endl;
+    identify_pointer(new A);
+    identify_pointer(new B);
+    identify_pointer(new C);
+    std::cout << "/***************** identify Referende ***********************/" << std::endl;
+    identify_refernce(*(new A));
+    identify_refernce(*(new B));
+    identify_refernce(*(new C));
     return 0;
 }
