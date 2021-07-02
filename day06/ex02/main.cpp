89426b5a52c2106156878529a95d9f8d26d99e99
ahaddad98 <amine.haddadd@gmail.com>
2021-07-02 13:12:48 +0100

    day06 ex02 just add main

--- day06/ex02/main.cpp
+++ day06/ex02/main.cpp
@@ -6,14 +6,14 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 16:41:55 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/01 18:52:58 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/02 13:12:24 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-# include "Base.hpp"
+#include "Base.hpp"
 
-Base * generate(void)
+Base *generate(void)
 {
     srand(time(0));
     int r = rand() % 3;
@@ -26,20 +26,67 @@ Base * generate(void)
     return NULL;
 }
 
-// void identify(Base* p)
-// {
-    
-// }
+void identify(Base *p)
+{
+    A *_a = dynamic_cast<A *>(p);
+    if (_a != NULL)
+    {
+        std::cout << "A" << std::endl;
+        return;
+    }
+    B *_b = dynamic_cast<B *>(p);
+    if (_b != NULL)
+    {
+        std::cout << "B" << std::endl;
+        return;
+    }
+    C *_c = dynamic_cast<C *>(p);
+    if (_c != NULL)
+    {
+        std::cout << "C" << std::endl;
+        return;
+    }
+    std::cout << "Not A not B and not C" << std::endl;
+}
 
-// void identify(Base& p)
-// {
-    
-// }
+void identify(Base &p)
+{
+    try
+    {
+        A &_a = dynamic_cast<A &>(p);
+        std::cout << "A" << std::endl;
+        ;
+    }
+    catch (std::bad_cast &e)
+    {
+        std::cerr << e.what() << std::endl;
+        ;
+    }
+    try
+    {
+        B &_b = dynamic_cast<B &>(p);
+        std::cout << "B" << std::endl;
+        ;
+    }
+    catch (std::bad_cast &e)
+    {
+        std::cerr << e.what() << std::endl;
+        ;
+    }
+    try
+    {
+        C &_c = dynamic_cast<C &>(p);
+        std::cout << "C" << std::endl;
+        ;
+    }
+    catch (std::bad_cast &e)
+    {
+        std::cerr << e.what() << std::endl;
+        ;
+    }
+}
 
 int main()
 {
-    Base *b = NULL;
-    b = generate();
-    std::cout << "amine haddad" << std::endl;
     return 0;
 }
