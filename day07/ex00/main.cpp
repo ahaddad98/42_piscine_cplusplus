fc52c3127d171c733884b6b77a6aae756a226c4a
Amine Haddad <ahaddad@Amines-MacBook-Air.local>
2021-07-04 00:20:31 +0100

    day07 ex00 DONE

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -6,7 +6,24 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 00:39:17 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/03 00:39:18 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/04 00:17:54 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -6,7 +6,33 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 00:38:48 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/03 00:38:49 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/04 00:19:56 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+# include <iostream>
+
+template<typename T>
+T max(T const &x , T const & y)
+{
+    return ((x>y) ? x : y);
+}
+
+template<typename T>
+T min(T const &x , T const & y)
+{
+    return ((x<y) ? x : y);
+}
+
+template<typename T>
+void swap(T  &x , T  & y)
+{
+    T  temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
+#endif
