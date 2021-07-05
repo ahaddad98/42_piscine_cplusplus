bdeb01591d8b86d393339ccae105bc442e26a751
ahaddad98 <amine.haddadd@gmail.com>
2021-07-05 16:53:26 +0100

    day07 ex02 error in copy contructor

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,46 +6,107 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 00:55:18 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/04 23:44:51 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/05 16:51:27 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
-# define ARRAY_HPP
-# include <iostream>
+#define ARRAY_HPP
+#include <iostream>
+#include <exception>
 
-
-template<typename T>
-
+template <typename T>
 class Array
 {
 private:
-    unsigned int length;
+    int length;
     T *_array;
-public:
-    Array<T>(/* args */);
-    Array<T>(unsigned int _n);
-    Array<T>(Array<T> const & T);
-    Array<T> &operator=(Array<T> const & T);
-    int size() const {
-    return length;
-    }
-    ~Array<T>();
-};
-template<typename T>
-Array<T>::Array() : _array(new T[0]) , length(0)
-{
-}
 
-template<typename T>
-Array<T>::Array(unsigned int _n) : _array( new T(_n)) , length(_n)
-{
-    int i = 0;
-    while (i < length)
+public:
+    /****************************exception od operator []*****************************/
+    class Errorinindex : public std::exception
     {
-        _array[i] = T();
-        i++;
+        virtual const char *what() const throw()
+        {
+            return "error in index";
+        }
+    };
+
+    /*****************************constructors**************************************/
+    Array<T>(/* args */) : length(0), _array(nullptr)
+    {
     }
+
+    Array<T>(unsigned int _n) : length(_n), _array(new T(length))
+    {
+        int i = 0;
+        while (i < length)
+        {
+            _array[i] = 0;
+            i++;
+        }
+    }
+    /***************************operator= and copy constructor***********************/
+    Array<T>(Array<T> const &array)
+    {
+        *this = array;
+    }
+    Array<T> &operator=(Array<T> const &array)
+    {
+        if (this != &array)
+        {
+            // if (length != array.length)
+            // {
+            //     if (_array != nullptr)
+            //     {
+            //         // delete[] _array;
+            //         // std::cout << "amine haddad" << std::endl;
+            //         length = 0;
+            //         _array = nullptr;
+            //     }
+                _array = new T[array.length];
+                int i = 0;
+                while (i < array.length)
+                {
+                    _array[i] = array[i];
+                    i++;
+                }
+            // }
+        }
+        return *this;
+    }
+    /***************************operatot [] ******************************************/
+    T &operator[](int index)
+    {
+        if (index < 0 || index > length)
+            throw Errorinindex();
+        else
+            return _array[index];
+    }
+    T const &operator[](int index) const
+    {
+        if (index < 0 || index >= length)
+            throw Errorinindex();
+        else
+            return _array[index];
+    }
+    /***************************size to get the size of the array*********************/
+    int size() const
+    {
+        return length;
+    }
+    /***************************** destrucor *****************************************/
+    ~Array<T>()
+    {
+            delete[] _array;
+    }
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, Array<T> const &p)
+{
+    out << "the size of array is " << p.size() << std::endl;
+    return out;
 }
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -6,14 +6,60 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 00:55:51 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/04 14:05:23 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/05 16:44:30 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Array.hpp"
 
-int main()
+#define MAX_VAL 750
+int main(int, char**)
 {
-    std::cout << "amine haddad" << std::endl;
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        // Array<int> test(tmp);
+    }
+
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     if (mirror[i] != numbers[i])
+    //     {
+    //         std::cerr << "didn't save the same value!!" << std::endl;
+    //         return 1;
+    //     }
+    // }
+    // try
+    // {
+    //     numbers[-2] = 0;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
+    // try
+    // {
+    //     numbers[MAX_VAL] = 0;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
+
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     numbers[i] = rand();
+    // }
+    // delete [] mirror;
     return 0;
 }
