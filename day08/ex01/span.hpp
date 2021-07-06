94b85ef2b24141f3d0b9a56278a9ebca615ea141
ahaddad98 <amine.haddadd@gmail.com>
2021-07-06 20:56:15 +0100

    day08 ex01 ...

--- day08/ex01/span.hpp
+++ day08/ex01/span.hpp
@@ -6,7 +6,7 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 12:42:57 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/06 12:44:53 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/06 20:49:59 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 class span
 {
 private:
-    /* data */
+    unsigned int n;
 public:
     span(/* args */);
+    span(span const &src);
+    span &operator=(span const &src);
+    span(unsigned int _n);
     ~span();
 };
 
