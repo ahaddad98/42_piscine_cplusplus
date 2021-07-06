e9f41a05d466a7f354e7e2db8e7724e7a5846c31
Amine Haddad <ahaddad@Amines-MacBook-Air.local>
2021-07-06 19:42:42 +0100

    day08 ex00 ...

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -6,13 +6,19 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 12:39:58 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/06 12:41:26 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/06 18:53:09 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 #include <iostream>
+#include <algorithm>
 
+template<typename T>
+void easyfind(T container, int idx)
+{
+    // std::find(container)    
+}
 
 #endif
