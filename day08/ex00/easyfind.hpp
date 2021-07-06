9ff751b3600b041ed0a841bccc180da5d1209c80
ahaddad98 <amine.haddadd@gmail.com>
2021-07-06 20:11:27 +0100

    day08 ex00 done

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 12:39:58 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/06 18:53:09 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/06 20:08:24 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 #include <algorithm>
 
 template<typename T>
-void easyfind(T container, int idx)
+int easyfind(T container, int to_find)
 {
-    // std::find(container)    
+    if (std::find(container.begin() , container.end() , to_find) ==  container.end())
+        return 1;
+    return 0;
 }
 
 #endif
--- day08/ex00/main.cpp
+++ day08/ex00/main.cpp
@@ -6,14 +6,27 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 12:40:29 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/07/06 12:41:17 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/07/06 20:10:59 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
 
 int main()
 {
-    std::cout << "amine haddad" << std::endl;
+    std::vector<int> v(5);
+    int i = 0;
+    while (i < (int)v.size())
+    {
+        v[i] = i+1;
+        i++;
+    }
+    if (easyfind(v, 0) == 0)
+    {
+        std::cout << "there is the element on the vecotr" << std::endl;
+    }
+    else
+        std::cout << "there is not  element in the vecotr" << std::endl;
     return 0;
 }
