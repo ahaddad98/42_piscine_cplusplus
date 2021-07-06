4cf6908213fb2073a5525749c5fb7120d018ffc8
Amine Haddad <ahaddad@Amines-MacBook-Air.local>
2021-07-06 12:48:52 +0100

    day08 .... here we goo

---
+++ day08/ex02/mutantstack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 12:46:30 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/07/06 12:48:10 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK
+# define MUTANTSTACK
+#include <iostream>
+class mutantstack
+{
+private:
+    /* data */
+public:
+    mutantstack(/* args */);
+    ~mutantstack();
+};
+
+
+#endif
